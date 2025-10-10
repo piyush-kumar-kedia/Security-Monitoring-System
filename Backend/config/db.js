5c55a8af346221cd9088e804a4dd1e8c67762994
Jayansh <jdgupta1980@gmail.com>
2025-10-10 20:41:12 +0530

    backend updated

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -16,17 +16,4 @@ pool.connect()
   .then(() => console.log("Connected to PostgreSQL"))
   .catch(err => console.error("Connection error", err.stack));
 
-// const connectDB = async () => {
-//     try {
-//         const conn = await mongoose.connect(process.env.MONGO_URI, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         });
-//         console.log(`MongoDB Connected: ${conn.connection.host}`);
-//     } catch (error) {
-//         console.error(`Error: ${error.message}`);
-//         process.exit(1);
-//     }
-// };
-
 module.exports = pool;
